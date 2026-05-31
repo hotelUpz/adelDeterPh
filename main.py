b4c6f49f46a745529c79d57743ea97179e6aa733
Sonnik9 <nikolas.smsttt@gmail.com>
2026-05-31 03:55:31 +0300

    before depl railw

--- main.py
+++ main.py
@@ -7,3 +7,25 @@ if __name__ == "__main__":
         asyncio.run(start_app())
     except KeyboardInterrupt:
         print("\nProcess terminated by user.")
+
+
+# # chmod 600 ssh_key.txt
+# # eval "$(ssh-agent -s)" 
+# # ssh-add ssh_key.txt
+# # source .ssh-autostart.sh
+# # git push --set-upstream origin master
+# # git config --global push.autoSetupRemote true
+# # ssh -T git@github.com 
+# # git log -1
+
+# # git add .
+# # git commit -m "plh37"
+# # git push
+
+# # pip install anthropic
+# # npm install -g @anthropic-ai/claude-code
+
+# # export ANTHROPIC_API_KEY=...
+# # claude
+
+# taskkill /F /IM python.exe
